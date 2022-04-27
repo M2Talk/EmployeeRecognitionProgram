eb1126e76bca23c7030cc6f99e63876cd2290f2d
M2Talk <mtalkowski1903@gmail.com>
2022-04-27 17:03:36 +0200

    Update kudo_spec.rb

--- spec/system/kudo_spec.rb
+++ spec/system/kudo_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Kudo CRUD test', type: :system do
   let!(:employee) { create(:employee) }
+  let!(:employee1) { create(:employee) }
+
 
   context 'when want to CRUD kudo' do
     before do
@@ -16,9 +18,9 @@ RSpec.describe 'Kudo CRUD test', type: :system do
 
       fill_in 'kudo_title', with: 'Test title'
       fill_in 'kudo_content', with: 'Test content'
-
+      select employee1.email, from: 'kudo_receiver_id'
+      
       click_button 'Create Kudo'
-
       expect(page).to have_content 'Kudo was successfully created.'
 
       # edit kudo
