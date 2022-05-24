1e77804c591762383a9826d2a09770138abf058b
M2Talk <mtalkowski1903@gmail.com>
2022-05-24 10:36:30 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  private
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || admin_root_path
+  end
 end
